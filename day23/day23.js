6712a5bd7aa0f0e88ab42a7b2ad57e7374318fe5
Ákos Oláh <akoso@spotify.com>
2023-12-23 22:59:47 +0100

    Added some recursion

--- day23/day23.js
+++ day23/day23.js
@@ -3,20 +3,94 @@ import { Graph } from "graphlib";
 import { fileReader } from "../shared/fileReader.js";
 import { createMatrix } from "../day3/day3.js";
 
-const populateGraph = (g, data) => {
-    data.forEach((line, j) => {
+const condSetEdges = (g, n, m) => {
+    if (g.hasNode(`${m.i},${m.j}`)) {
+        g.setEdge(`${n.i},${n.j}`, `${m.i},${m.j}`);
+        g.setEdge(`${m.i},${m.j}`, `${n.i},${n.j}`);
+    }
+};
+
+const addEdges = (g) => {
+    g.nodes().forEach((node) => {
+        const nodeData = g.node(node);
+        condSetEdges(g, nodeData, { i: nodeData.i - 1, j: nodeData.j });
+        condSetEdges(g, nodeData, { i: nodeData.i, j: nodeData.j - 1 });
+        condSetEdges(g, nodeData, { i: nodeData.i + 1, j: nodeData.j });
+        condSetEdges(g, nodeData, { i: nodeData.i, j: nodeData.j + 1 });
+    });
+};
+
+const addNodes = (g, mtx) => {
+    mtx.forEach((line, j) => {
         line.forEach((item, i) => {
             if (item === ".") g.setNode(`${i},${j}`);
+            switch (item) {
+                case ".":
+                    g.setNode(`${i},${j}`, {
+                        i,
+                        j,
+                    });
+                    break;
+                case "<":
+                    g.setEdge(`${i + 1},${j}`, `${i - 1},${j}`);
+                    break;
+                case ">":
+                    g.setEdge(`${i - 1},${j}`, `${i + 1},${j}`);
+                    break;
+                case "v":
+                    g.setEdge(`${i},${j - 1}`, `${i},${j + 1}`);
+                    break;
+                case "^":
+                    g.setEdge(`${i},${j + 1}`, `${i},${j - 1}`);
+                    break;
+                default:
+                    break;
+            }
         });
     });
-    return g;
+};
+
+const populateGraph = (g, mtx) => {
+    addNodes(g, mtx);
+    addEdges(g);
+};
+
+const walkTheGraph = (g, cursor, len) => {
+    if (g.node(cursor) && g.node(cursor).visited) {
+        return len;
+    }
+
+    g.setNode(cursor, {
+        ...g.node(cursor),
+        visited: true,
+    });
+
+    const edges = g.outEdges(cursor);
+    if (edges.length === 0) return len;
+
+    let maxWeight = 0;
+    edges.forEach((edge) => {
+        if (!g.node(edge.w) || !g.node(edge.w).visited) {
+            const getWeight = walkTheGraph(g, edge.w, len + 1);
+            maxWeight = Math.max(maxWeight, getWeight);
+            g.setEdge(edge.v, edge.w, {
+                weight: getWeight,
+            });
+        }
+        console.log(edge);
+    });
+    return maxWeight;
+    // g.setEdge(cursor)
+    // console.log(g.outEdges(cursor));
 };
 
 const main = async () => {
     let g = new Graph();
     const data = await fileReader("./day3/day3.input.txt");
     const mtx = createMatrix(data);
-    g = populateGraph(g, mtx);
+    populateGraph(g, mtx);
+    const longest = walkTheGraph(g, "1,0", 0);
+    console.log(longest);
     // g.setNode("0,0");
     // console.log(g.hasNode("0,0"));
     // console.log(g.hasNode("0,1"));
@@ -24,4 +98,4 @@ const main = async () => {
 
 if (process.env.NODE_ENV !== "test") main();
 
-export { populateGraph };
+export { populateGraph, walkTheGraph };
--- day23/day23.test.js
+++ day23/day23.test.js
@@ -1,20 +1,61 @@
 import { describe, expect, test } from "@jest/globals";
 import { Graph } from "graphlib";
 import { createMatrix } from "../day3/day3";
-import { populateGraph } from "../day23/day23";
+import { populateGraph, walkTheGraph } from "../day23/day23";
 
 describe("populateGraph", () => {
-    test.only("create the right nodes", () => {
+    test("create the right nodes", () => {
         const testData = `#.##
 #..#
 .<..
 .###
 `;
-
-        let g = new Graph();
+        const nodes = ["1,0", "1,1", "2,1", "0,2", "2,2", "3,2", "0,3"];
+        const g = new Graph();
         const mtx = createMatrix(testData);
         populateGraph(g, mtx);
-        console.log(g.nodes());
-        // expect(testResult.nodes).toBe(3);
+        expect(g.nodes()).toEqual(nodes);
+    });
+});
+
+describe("walkTheGraph", () => {
+    test("Basic Recursion", () => {
+        const g = new Graph();
+        g.setNode("a");
+        g.setEdge("a", "b");
+        g.setEdge("b", "c");
+        g.setEdge("b", "d");
+        g.setEdge("d", "e");
+        g.setEdge("e", "f");
+        g.setEdge("b", "f");
+        const longest = walkTheGraph(g, "a", 0);
+        expect(g.edge("a", "b")).toEqual({ weight: 4 });
+        expect(longest).toEqual(4);
+    });
+    test("Visited flagged", () => {
+        const g = new Graph();
+        g.setNode("a");
+        g.setEdge("a", "b");
+        g.setEdge("b", "c");
+        g.setEdge("b", "d");
+        g.setEdge("d", "e");
+        g.setEdge("e", "f");
+        g.setEdge("b", "f");
+        const longest = walkTheGraph(g, "a", 0);
+        expect(g.node("f")).toEqual({ visited: true });
+    });
+    test("Both Ways", () => {
+        const g = new Graph();
+        g.setNode("a");
+        g.setEdge("a", "b");
+        g.setEdge("b", "a");
+        g.setEdge("b", "c");
+        g.setEdge("c", "b");
+        g.setEdge("b", "d");
+        g.setEdge("d", "e");
+        g.setEdge("e", "f");
+        g.setEdge("b", "f");
+        const longest = walkTheGraph(g, "a", 0);
+        expect(longest).toEqual(4);
     });
 });
